855c7e994cbac52e13642ed2eafcef3cd4589ccc
AlKasatov <slabass@mail.ru>
2024-05-03 14:00:05 +0600

    add gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://alkasatov.github.io/drum-machine/',
   plugins: [
     react(),
     splitVendorChunkPlugin(),
